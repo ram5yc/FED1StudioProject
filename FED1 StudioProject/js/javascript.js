0493d646762ac30f01d873dee45cb8b84e35f91c
Rachel Mantei <ram5yc@mail.missouri.edu>
2021-01-14 15:58:13 -0600

    committ

---
+++ FED1 StudioProject/js/javascript.js
@@ -0,0 +1,17 @@
+﻿$(".thumb").on("click", function () {
+    //Find the src attribute of the img tag that was clicked and store that as a variable 
+    var imgSrc = $(this).attr("src"); //src attribute is the photo contained in the var imgSrc variable
+
+    //Pug an <img> into the #lightbox-content
+    $("#lightbox-content").html('<img src="' + imgSrc + '" class="img-responsive"/>');
+
+    //Make the lightbox visible by fading in
+    $("#lightbox-container").fadeIn(500);
+
+    //When the user clicks anywhere in the lightbox-container, fadeOut()
+    $("#lightbox-container").on("click", function () {
+        $(this).fadeOut(500);
+    });
+
+});
+
