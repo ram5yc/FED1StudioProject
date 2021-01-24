90d07b7a4ad388aab773cb2fbfd9bb0e69f63f20
Rachel Mantei <ram5yc@mail.missouri.edu>
2021-01-24 14:13:59 -0600

    commit

--- FED1 StudioProject/js/javascript.js
+++ FED1 StudioProject/js/javascript.js
@@ -1,4 +1,4 @@
-﻿
+﻿/*S
 ///<reference path="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"/>
 $(".thumb").on("click", function () {
     //Find the src attribute of the img tag that was clicked and store that as a variable 
@@ -15,5 +15,21 @@ $(".thumb").on("click", function () {
         $(this).fadeOut(500);
     });
 
+}); */
+
+var map;
+
+function initMap(){
+//map options
+map = new google.maps.Map(document.getElementById("map"), {
+center:  {lat: 38.9667, lng: 94.6169},
+zoom: 4
 });
 
+
+//add marker
+const marker = new google.maps.Marker({
+    position: {lat: 38.963570, lng: -94.607162},
+    map: map,
+});
+}
