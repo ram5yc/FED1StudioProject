17c9f2bd9505c660d70bbdbb1783c154076d0614
Rachel Mantei <ram5yc@mail.missouri.edu>
2021-01-30 16:19:43 -0600

    commit

--- FED1 StudioProject/js/javascript.js
+++ FED1 StudioProject/js/javascript.js
@@ -1,5 +1,7 @@
-﻿/*S
+﻿
 ///<reference path="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"/>
+
+
 $(".thumb").on("click", function () {
     //Find the src attribute of the img tag that was clicked and store that as a variable 
     var imgSrc = $(this).attr("src"); //src attribute is the photo contained in the var imgSrc variable
@@ -15,6 +17,4 @@ $(".thumb").on("click", function () {
         $(this).fadeOut(500);
     });
 
-}); */
-
-}
+});
